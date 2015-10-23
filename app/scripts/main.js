61589070984c954f9b63079e2dd9efb6769493df
Nick Mitchell <nick@fluency.com.au>
2015-10-23 18:18:26 +1100

    adding threes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,61 @@
-console.log('\'Allo \'Allo!');
+$(function(){
+	console.log('init');
+
+	var scene = new THREE.Scene();
+	var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+	var renderer = new THREE.WebGLRenderer();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	document.body.appendChild( renderer.domElement );
+
+	var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+	var material = new THREE.MeshBasicMaterial( { color: 0x333333 } );
+	var cube = new THREE.Mesh( geometry, material );
+
+	var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+	directionalLight.position.set( 0, 4, 0 );
+	scene.add( directionalLight );
+
+	scene.add( cube );
+
+	camera.position.z = 5;
+
+	function render() {
+
+		requestAnimationFrame( render );
+
+		cube.rotation.x += 0.01;
+		cube.rotation.y += 0.01;
+
+		renderer.render( scene, camera );
+
+	}
+	render();
+
+
+	//stats
+	var stats = new Stats();
+	stats.setMode( 0 ); // 0: fps, 1: ms, 2: mb
+
+	// align top-left
+	stats.domElement.style.position = 'absolute';
+	stats.domElement.style.left = '0px';
+	stats.domElement.style.top = '0px';
+
+	document.body.appendChild( stats.domElement );
+
+	var update = function () {
+
+	    stats.begin();
+
+	    // monitored code goes here
+
+	    stats.end();
+
+	    requestAnimationFrame( update );
+
+	};
+
+	requestAnimationFrame( update );
+
+});
